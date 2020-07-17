4976f1c37062aeb693f3bb843e06f5f1beff353a
David Munoz Constantine <dmunoz.dmc@gmail.com>
2020-07-17 09:06:20 -0700

    added few functions and tests for graph

--- Open-Source-Soldier-of-Fortune/dmunozc-submission/graph.py
+++ Open-Source-Soldier-of-Fortune/dmunozc-submission/graph.py
@@ -11,67 +11,143 @@ chapter 13.
 
 import heapq
 
-class Graph:
 
+class Graph:
     def __init__(self, vertices):
         """Stores vertices by name and values.
 
         Examples
         The following vertices
-           | A  |  B
-        A  | 1  |  5
-        B  | 3  |  -1
+        *      **FROM**
+        *    | A | B
+        T  A | 1 | 5
+        O  B | 3 | -1
+        *
+        *
         Are stored in a dictionary as
         dict[A] = [(1, A, A), (5, A, B)]
         dict[B] = [(3, B, A), (-1, B, B)]
         Noting that the underlying order is kept.
-        """
-        self.vertices = {}
-        for vertex in vertices:
-            self.vertices[vertex] = [(edge[dest], vertex, dest) for dest, edge in vertices[vertex].groupby(level=0)]
 
-    def smallest_edge(self, edges, visited, not_visited):
-        """Finds the smallest cost edge (u, v) that satisfies the following
-        conditions:
-        - u is in visited
-        - v is not visited
-        - edge has smallest cost
+        Parameters
+        ----------
+        vertices: pandas dataframe
+            dataframe that contains the distances of a graph.
+        """
+        self.graph_dict = {}
+        for vertex in vertices:
+            self.graph_dict[vertex] = [
+                (edge[dest], vertex, dest)
+                for dest, edge in vertices[vertex].groupby(level=0)
+                if edge[dest] != 0
+            ]
+
+    def vertices(self):
+        """Returns the vertices as a list.
+
+        Returns
+        -------
+        vertices: list
+        """
+        return list(self.graph_dict.keys())
+
+    def edges(self):
+        """Returns list of unweighted edges.
+
+        Returns
+        -------
+        edges: list of edge tuples
+        """
+        return self.__generate_edges(directed=True, weights=False)
+
+    def weighted_edges(self):
+        """Returns list of weighted edges.
+
+        Returns
+        -------
+        edges: list of edge tuples
+        """
+        return self.__generate_edges(directed=True, weights=True)
+
+    def __generate_edges(self, directed=True, weights=False):
+        """Returns list of edges, with or without weights, directed or
+        undirected.
+
+        Returns
+        -------
+        edges: list of edge tuples
+        """
+        edges = []
+        i = 0
+        for vertex in self.graph_dict:
+            for j in range(i, len(self.graph_dict[vertex])):
+                edge = self.graph_dict[vertex][j]
+                # Remove the weights if not required.
+                if not weights:
+                    edge = edge[1:]
+                edges.append(edge)
+            # If the graph is undirected, skip the symmetry.
+            if not directed:
+                i += 1
+        return edges
+
+    def __smallest_edge(self, edges, visited, not_visited):
+        """Finds the smallest cost edge (u, v) for creating an MST.
+
+        It satisfies the following conditions:
+        - origin is in visited.
+        - destination is not visited.
+        - edge has smallest cost.
+
+        Parameters
+        ----------
+        edges : list of tuples
+            a priority queue list that contains the edges tuple.
+            edges tuple consists of (cost, origin, destination).
+        visited: set
+            a set that contains the currently visited nodes.
+        not_visited: set
+            a set that contains the currently not visited nodes.
+        Returns
+        -------
+        edge : tuple
+            tuple that contains the edge information.
+            tuple consists of (cost, origin, destination)
         """
         found = False
         old_edges = edges.copy()
         while not found:
             edge = heapq.heappop(old_edges)
-            if edge[1] in visited and edge[2] in not_visited:
+            if any(v in visited for v in edge[1:]) and \
+                    any(v in not_visited for v in edge[1:]):
                 found = True
         return edge
 
-
-
-
     def mst(self):
-        """Returns minimum spanning tree in a dictionary."""
+        """Returns minimum spanning tree in a dictionary.
+
+        Vanilla Prim's algorithm only works for an undirected graph.
+        Therefore this implementation assumes symmetry of adjacency matrix
+
+        Returns
+        ----------
+        spanning_tree: set
+            set that contains the minimum spanning tree edges.
+        """
+        # Find all distinct edges. Assumed an undirected graph.
+        edges = self.__generate_edges(directed=False, weights=True)
         # Presort edges by using a priority queue.
-        edges = set()
-        for vertex in self.vertices:
-            for edge in self.vertices[vertex]:
-                # If to and destination are different.
-                if edge[1] != edge[2]:
-                    edges.add(edge)
-        edges = list(edges)
         heapq.heapify(edges)
+        # Use the lowest cost edge as the starting point.
         edge = heapq.heappop(edges)
-        visited = set([edge[1], edge[2]])
-        not_visited = set([k for k in self.vertices])
-        not_visited.remove(edge[1])
-        not_visited.remove(edge[2])
+        visited = set(edge[1:])
+        not_visited = set([k for k in self.graph_dict])
+        not_visited.difference_update(edge[1:])
         spanning_tree = set([edge])
+        # Create the mst based on lowest cost edges.
         while not_visited:
-            edge = self.smallest_edge(edges, visited, not_visited)
-            visited.add(edge[2])
-            not_visited.remove(edge[2])
+            edge = self.__smallest_edge(edges, visited, not_visited)
+            visited.update(edge[1:])
+            not_visited.difference_update(edge[1:])
             spanning_tree.add(edge)
         return spanning_tree
-
-
-
-
--- Open-Source-Soldier-of-Fortune/dmunozc-submission/test_mst.py
+++ Open-Source-Soldier-of-Fortune/dmunozc-submission/test_mst.py
@@ -4,38 +4,100 @@
 import pytest
 from graph import Graph
 import pandas as pd
-import numpy as np
+import itertools
+
 
 @pytest.fixture
-def small_distances():
+def few_names():
+    return ["A", "B", "C"]
+
+
+@pytest.fixture
+def few_distances():
+    return [[1, 2, 3], [4, 5, 6], [-7, -8, -9]]
+
+
+@pytest.fixture
+def few_graph(few_distances, few_names):
+    df = pd.DataFrame(few_distances, columns=few_names, index=few_names)
+    return Graph(df)
+
+
+@pytest.fixture
+def book_names():
+    return ["CHV", "GE", "KO", "PG", "TX", "XON"]
+
+
+@pytest.fixture
+def book_distances():
     """Mantenga, Stanley chapter 13 example"""
-    return [[0,    1.15, 1.18, 1.15, 0.84, 0.89],
-            [1.15, 0,    0.86, 0.89, 1.26, 1.16],
-            [1.18, 0.86, 0,    0.74, 1.27, 1.11],
-            [1.15, 0.89, 0.74, 0,    1.26, 1.10],
-            [0.84, 1.26, 1.27, 1.26, 0,    0.94],
-            [0.89, 1.16, 1.11, 1.10, 0.94, 0   ]]
+    return [
+        [0, 1.15, 1.18, 1.15, 0.84, 0.89],
+        [1.15, 0, 0.86, 0.89, 1.26, 1.16],
+        [1.18, 0.86, 0, 0.74, 1.27, 1.11],
+        [1.15, 0.89, 0.74, 0, 1.26, 1.10],
+        [0.84, 1.26, 1.27, 1.26, 0, 0.94],
+        [0.89, 1.16, 1.11, 1.10, 0.94, 0],
+    ]
 
 
-def test_mst_book(small_distances):
+@pytest.fixture
+def book_graph(book_distances, book_names):
+    """Creates the graph based on the other fixtures."""
+    df = pd.DataFrame(book_distances, columns=book_names, index=book_names)
+    return Graph(df)
+
+
+def test_graphs(book_distances, book_names, few_distances, few_names):
+    """Tests that the produce graph matches the matrix."""
+    for dst, names in [
+        (book_distances, book_names),
+        (few_distances, few_names),
+    ]:
+        df = pd.DataFrame(dst, columns=names, index=names)
+        graph = Graph(df)
+        # For each vertex value in the graph, check it against the matrix.
+        # Since distance of 0 is ignored in the graph, need to offset it if
+        # present.
+        for i in range(len(names)):
+            offset = 0
+            vertex = names[i]
+            for j in range(len(graph.graph_dict[vertex])):
+                if dst[j][i] == 0:
+                    offset = -1
+                else:
+                    assert graph.graph_dict[vertex][j + offset][0] == dst[j][i]
+
+
+def test_vertices(book_graph, book_names, few_graph, few_names):
+    """Tests that the created graph contains the vertices it was created
+    with."""
+    for graph, names in [(book_graph, book_names), (few_graph, few_names)]:
+        vertices = graph.vertices()
+        assert names == vertices
+
+
+def test_few_edges(few_graph, few_names):
+    """Tests the undirected edges are present from the created graph."""
+    all_edges = [p for p in itertools.product(few_names, repeat=2)]
+    graph_edges = few_graph.edges()
+    assert len(graph_edges) == len(all_edges)
+    assert graph_edges == all_edges
+
+
+def test_book_mst(book_distances, book_names):
     """Tests the MST produces matches Stanley's book.
 
     Note that MSTs are not unique, so there is always a possibility that
     exact matching answers are not possible.
     """
     # From Stanley chapter 13
-    names = ["CHV", "GE", "KO", "PG", "TX", "XON"]
-    df = pd.DataFrame(small_distances, columns=names, index=names)
+    df = pd.DataFrame(book_distances, columns=book_names, index=book_names)
     graph = Graph(df)
     mst = graph.mst()
     mst = sorted(mst)
-    assert(mst[0][0] == 0.74 and "KO"  in mst[0] and "PG"  in mst[0])
-    assert(mst[1][0] == 0.84 and "CHV" in mst[1] and "TX"  in mst[1])
-    assert(mst[2][0] == 0.86 and "KO"  in mst[2] and "GE"  in mst[2])
-    assert(mst[3][0] == 0.89 and "CHV" in mst[3] and "XON" in mst[3])
-    assert(mst[4][0] == 1.10 and "PG"  in mst[4] and "XON" in mst[4])
-
-
-
-
-
+    assert mst[0][0] == 0.74 and "KO" in mst[0] and "PG" in mst[0]
+    assert mst[1][0] == 0.84 and "CHV" in mst[1] and "TX" in mst[1]
+    assert mst[2][0] == 0.86 and "KO" in mst[2] and "GE" in mst[2]
+    assert mst[3][0] == 0.89 and "CHV" in mst[3] and "XON" in mst[3]
+    assert mst[4][0] == 1.10 and "PG" in mst[4] and "XON" in mst[4]
